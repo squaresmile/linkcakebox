cc045e826fe18a97fdbd2ee848cd2c8e7a3af801
squaresmile <squaresmile@users.noreply.github.com>
2017-07-19 13:36:23 +0700

    remove unused bugurl, add cosmetic

--- init.js
+++ init.js
@@ -9,12 +9,6 @@ if(plugin.canChangeMenu())
 {
     theWebUI.urlTabcakebox = function(dID,fno)
     {
-        function bugurl (url)
-        {
-            url = url.replace("'","%27");
-            return url;
-        }
-
         var base_path = this.torrents[dID].base_path;
         if (this.files[dID][fno].name != this.torrents[dID].name)
             base_path = base_path + "/" + this.files[dID][fno].name;
@@ -22,7 +16,7 @@ if(plugin.canChangeMenu())
         base_path = base_path.replace(plugin.dirpath, "");
         var cakeboxUrl = "'" + plugin.url + base_path + "'";
         return cakeboxUrl;
-    }
+    };
   
     plugin.createFileMenu = theWebUI.createFileMenu;
     theWebUI.createFileMenu = function( e, id )
@@ -47,12 +41,12 @@ if(plugin.canChangeMenu())
                             fno = fid.substr(3);
                     }
                 }
-                theContextMenu.add( [theUILang.linkcakeboxmenu, (fno == null) ? null : plugin.optionlink + "(" + theWebUI.urlTabcakebox(theWebUI.dID, fno) + ")"] );
+                theContextMenu.add( [theUILang.linkcakeboxmenu, (fno === null) ? null : plugin.optionlink + "(" + theWebUI.urlTabcakebox(theWebUI.dID, fno) + ")"] );
             }
             return(true);
         }
         return(false);
-    }
+    };
 }
 
 if(plugin.canChangeMenu()) {
@@ -63,31 +57,25 @@ if(plugin.canChangeMenu()) {
         plugin.createMenu.call(this, e, id);
         if(plugin.enabled)
         {
-            function bugurl (url)
-            {
-                url = url.replace("'","%27");
-                return url;
-            }
-
         	var base_path = this.torrents[id].base_path;
 //      	if (this.files[id][fno].name != this.torrents[id].name)
 //          base_path = base_path + "/" + this.files[id][fno].name;
         	base_path = base_path.replace(plugin.dirpath, "");
         	var cakeboxUrl = "'" + plugin.url + base_path + "'";
 //          var cakeboxUrl = "'" + plugin.url + bugurl(encodeURIComponent(theWebUI.torrents[id].base_path)) + "'";
-            theContextMenu.add( [theUILang.linkcakeboxmenu, (theWebUI.torrents[id].multi_file != 0 ) ? null : plugin.optionlink + "(" + cakeboxUrl + ")"] );
+            theContextMenu.add( [theUILang.linkcakeboxmenu, (theWebUI.torrents[id].multi_file !== 0 ) ? null : plugin.optionlink + "(" + cakeboxUrl + ")"] );
         }
-    }
+    };
 }
 
 plugin.onLangLoaded = function()
 {
     this.addButtonToToolbar("linkcakebox", theUILang.linkcakebox, plugin.optionlink+"('" + plugin.url + "')", "help");
     this.addSeparatorToToolbar("help");
-}
+};
 
 plugin.onRemove = function()
 {
     this.removeSeparatorFromToolbar("linkcakebox");
     this.removeButtonFromToolbar("linkcakebox");
-}
+};
